082458e691bb94a48af5be4f2a766521904d07aa
Achintha Shanuka Abeysinghe <shanukabro238@gmail.com>
2025-07-01 00:09:41 +0530

    feat: :art: yee

--- Frontend/src/index.jsx
+++ Frontend/src/index.jsx
@@ -3,8 +3,21 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles/tailwind.css";
 import "./styles/index.css";
+import { ClerkProvider } from '@clerk/clerk-react'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
+      <App />
+    </ClerkProvider>
+  </React.StrictMode>,
+)
