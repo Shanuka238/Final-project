f22a7a92594688d3d62726816d92e76f947b27e0
Shanuka <Shanukabro238@gmail.com>
2025-09-16 13:30:48 +0530

    feat: commit x

--- Frontend/src/api/profile.js
+++ Frontend/src/api/profile.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+const API_BASE = `${import.meta.env.VITE_API_URL}/api/user/profile`;
+
 export const updateUserProfile = async (data, token) => {
   const res = await axios.put(
-    '/api/user/profile',
+    API_BASE,
     data,
     { headers: { Authorization: `Bearer ${token}` } }
   );
@@ -10,7 +12,7 @@ export const updateUserProfile = async (data, token) => {
 };
 
 export const fetchUserProfile = async (token) => {
-  const res = await axios.get('/api/user/profile', {
+  const res = await axios.get(API_BASE, {
     headers: { Authorization: `Bearer ${token}` }
   });
   return res.data;
